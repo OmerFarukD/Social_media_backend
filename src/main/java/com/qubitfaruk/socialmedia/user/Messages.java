a3662839ea99be8b48a822d5795c63de31bab76f
OmerFarukD <omer.crlys.44@gmail.com>
2022-10-15 23:59:05 +0300

    Validation and BusinessRules implementation added Commit

--- src/main/java/com/qubitfaruk/socialmedia/user/Messages.java
+++ src/main/java/com/qubitfaruk/socialmedia/user/Messages.java
@@ -4,4 +4,6 @@ public class Messages {
     public static final String userSavedMessage="Kullanıcı eklendi.";
     public static final String userNotSavedMessage="Kullanıcı eklenemedi.";
     public static final String userAlreadyExists="Kullanıcı mevcuttur.";
+    public static final String usernameNotEmptyAndNullMessage="İsim  boş olamaz";
+    public static final String displayNameNotEmptyAndNullMessage="kullanıcı adı boş olamaz";
 }
--- src/main/java/com/qubitfaruk/socialmedia/user/UserController.java
+++ src/main/java/com/qubitfaruk/socialmedia/user/UserController.java
@@ -1,11 +1,14 @@
 package com.qubitfaruk.socialmedia.user;
 
+import com.qubitfaruk.socialmedia.core.error.ApiError;
 import com.qubitfaruk.socialmedia.core.results.DataResult;
-import com.qubitfaruk.socialmedia.core.results.Result;
-import com.qubitfaruk.socialmedia.core.results.SuccessResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("/api/1.0/user")
@@ -18,8 +21,27 @@ public class UserController {
     // http://localhost:8080/api/1.0/user/createuser
     @PostMapping("/createuser")
     @ResponseStatus(HttpStatus.CREATED)
-    public Result createUser(@RequestBody User user){
-       return this.userService.save(user);
+    public ResponseEntity<?> createUser(@RequestBody User user){
+
+        ApiError apiError=new ApiError(400,"validation error","/api/1.0/user/createuser");
+        Map<String,String> errors=new HashMap<String,String>();
+        String username= user.getUsername();
+        String displayName=user.getDisplayName();
+
+
+        if (username==null || username.isEmpty()){
+            errors.put("username",Messages.usernameNotEmptyAndNullMessage);
+        }
+        if (displayName==null){
+            errors.put("displayName",Messages.displayNameNotEmptyAndNullMessage);
+        }
+        if (errors.size()>0){
+            apiError.setValidationErrors(errors);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+        }
+        var data= this.userService.save(user);
+        if (!data.isSuccess()) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(data.getMessage());
+       return ResponseEntity.ok(data);
     }
 
     // http://localhost:8080/api/1.0/user/getByUsername
